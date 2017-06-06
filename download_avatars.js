ec589046a2e47c53ac8fd4c91213a142e93b2ad9
otto hu <ottohu101@gmail.com>
2017-06-06 00:48:16 +0000

    step 4

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
-
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+var GITHUB_USER = "aWildOtto";
+var GITHUB_TOKEN = "4699b7c7d8647420b412ba21701b5b65d0c39452";
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log("the url is: \n",requestURL);
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
