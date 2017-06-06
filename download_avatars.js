c42437be536f3fc1a4a7801806cb214b4260a8b4
otto hu <ottohu101@gmail.com>
2017-06-06 01:20:03 +0000

    step 5

--- download_avatars.js
+++ download_avatars.js
@@ -4,15 +4,22 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "aWildOtto";
 var GITHUB_TOKEN = "4699b7c7d8647420b412ba21701b5b65d0c39452";
-
+var USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log("the url is: \n",requestURL);
+
+  var options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': "Otto's awesome agent"
+    }
+  };
+  request(options,cb);
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function(err, result, body) {
+  console.log("Errors: ", err);
+  console.log("Result: ", result);
+  console.log("Body: ", body);
 });
